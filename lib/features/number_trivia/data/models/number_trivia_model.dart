3f78e3667b6ad315d2a76170c7276ef5b6ac9c2c
Thiago Lioy <thiagolioy@Thiagos-MacBook-Pro.local>
2023-05-16 15:46:07 -0300

    Fixture and models

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,15 @@
+import 'package:cleanarch_project/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  const NumberTriviaModel({required int number, required String text})
+      : super(number: number, text: text);
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+        number: (json["number"] as num).toInt(), text: json["text"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {"text": text, "number": number};
+  }
+}
--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,3 +1,4 @@
+import 'package:cleanarch_project/core/usecases/usecase.dart';
 import 'package:cleanarch_project/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:cleanarch_project/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:cleanarch_project/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
