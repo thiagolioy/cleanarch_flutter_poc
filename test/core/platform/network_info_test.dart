aefec2ff044f6c9f5836e41d7f33f443132197fe
Thiago Lioy <thiagolioy@Thiagos-MacBook-Pro.local>
2023-05-18 17:04:03 -0300

    Implement Network info

---
+++ test/core/platform/network_info_test.dart
@@ -0,0 +1,29 @@
+import 'package:cleanarch_project/core/platform/network_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockInternetConnectionChecker extends Mock
+    implements InternetConnectionChecker {}
+
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockInternetConnectionChecker mockInternetConnectionChecker;
+
+  setUp(() {
+    mockInternetConnectionChecker = MockInternetConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockInternetConnectionChecker);
+  });
+
+  group("isConnected", () {
+    test("should forward the call to InternetConnectionChecker.hasConnection",
+        () async {
+      final tHasConnectionFuture = Future.value(true);
+      when(() => mockInternetConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+      final result = networkInfo.isConnected;
+      verify(() => mockInternetConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
